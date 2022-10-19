a52cb3695809f22d8a5f00eac761aa6288bf59bc
Omar Lopez <omar@flywire.com>
2022-10-19 10:36:15 +0200

    WIP

--- scripts/build-postgres-with-wal-g-docker-image.sh
+++ scripts/build-postgres-with-wal-g-docker-image.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "$0")/../"
+#cd "$(dirname "$0")/../"
 
 ./scripts/build-wal-g-docker-image.sh
 
-docker build ./docker-image/postgres-with-wal-g -t stephaneklein/postgres-with-walg:12.2-alpine-walg0.2.15
+docker build ./docker-image/postgres-with-wal-g -t stephaneklein/postgres-with-walg:12.2-alpine-walg2.0.1
